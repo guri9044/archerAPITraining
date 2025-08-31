888351cfafd2254642f153aaa96470a1dc1691fc
Gurdas Singh <guri.9044@gmail.com>
2025-08-31 15:17:50 +0000

    session 4

--- ArcherAPI.py
+++ ArcherAPI.py
@@ -4,10 +4,11 @@ import requests
 class ArcherInstance:
     #default initialization method to set up instance variables and creating self object
     def __init__(self, inst_url, instance_name, username, password):
-        self.api_url_base = f"https://{inst_url}/platformapi/"
+        self.api_url_base = f"https://{inst_url}/"
         self.username = username
         self.password = password
         self.instance_name = instance_name
+        #self.session_token to store session token after login and be used in all other methods
         self.session_token = ""
         self.login()
         self.headers = {
@@ -18,7 +19,7 @@ class ArcherInstance:
     
     #login method to authenticate and get session token
     def login(self):
-        api_url = f"{self.api_url_base}core/security/login"
+        api_url = f"{self.api_url_base}platformapi/core/security/login"
         header = {"Accept": "application/json,text/html,application/xhtml+xml,application/xml;q =0.9,*/*;q=0.8",
 				  "Content-type": "application/json"}
         response = requests.post(api_url, headers=header, json={"InstanceName": self.instance_name,
@@ -35,7 +36,7 @@ class ArcherInstance:
     
     #create a user in Archer
     def createUser(self, userDetails):
-        response = requests.post(f"{self.api_url_base}core/system/user", headers=self.headers, json=userDetails, verify=False)
+        response = requests.post(f"{self.api_url_base}platformapi/core/system/user", headers=self.headers, json=userDetails, verify=False)
         data = json.loads(response.content)
         
         if response.status_code == 200:
@@ -47,10 +48,43 @@ class ArcherInstance:
     
     #assign role to user in Archer
     def assignRoleToUser(self, userRoleDetails):
-        response = requests.put(self.api_url_base+"core/system/userrole", headers=self.headers, json=userRoleDetails, verify=False)
+        response = requests.put(self.api_url_base+"platformapi/core/system/userrole", headers=self.headers, json=userRoleDetails, verify=False)
         data = json.loads(response.content)
         if response.status_code == 200:
             archerReqSuccess = data["IsSuccessful"]
         if archerReqSuccess:
             roleId = data["RequestedObject"]["Id"]
             print("Role with Id - ",roleId)
+
+    class Record:
+        def __init__(self, archer_instance):
+            self.archer_instance = archer_instance
+
+        def getRecordById(self, recordId):
+            response = requests.get(f"{self.archer_instance.api_url_base}platformapi/core/content/contentid?id={recordId}", headers=self.archer_instance.headers, verify=False)
+            data = json.loads(response.content)
+            #print(data)
+            if response.status_code == 200:
+                archerReqSuccess = data["IsSuccessful"]
+            if archerReqSuccess:
+                recordData = data["RequestedObject"]
+                #print("Record Data: ", recordData)
+                return recordData
+    class Fields:
+        def __init__(self, archer_instance):
+            self.archer_instance = archer_instance
+
+        def getFieldByAppId(self, appId):
+            response = requests.get(f"{self.archer_instance.api_url_base}platformapi/core/system/fielddefinition/application/{appId}", headers=self.archer_instance.headers, verify=False)
+            data = json.loads(response.content)
+            #print(data)
+            return data
+
+    class Content:
+        def __init__(self, archer_instance):
+            self.archer_instance = archer_instance
+
+        def getContentById(self, applicationName, contentId):
+            response = requests.get(f"{self.archer_instance.api_url_base}contentapi/{applicationName}({contentId})", headers=self.archer_instance.headers, verify=False)
+            data = json.loads(response.content)
+            print(data)
--- Integration1.py
+++ Integration1.py
@@ -1,8 +1,48 @@
 from ArcherAPI import ArcherInstance
 import os
 
+
 os.system('cls' if os.name == 'nt' else 'clear')
 
 archer = ArcherInstance("archer-irm.com/Archer", "training2025", "api.user", "Archer@123")
-userId = archer.createUser( {"User":{"FirstName":"FirstName","LastName":"LastName",},"Password":"Archer@123"})
-archer.assignRoleToUser({"UserId":userId,"RoleId":3,"IsAdd":'true'})
+#userId = archer.createUser( {"User":{"FirstName":"FirstName","LastName":"LastName",},"Password":"Archer@123"})
+#archer.assignRoleToUser({"UserId":userId,"RoleId":3,"IsAdd":'true'})
+
+recordsArray = [348575]
+FindingFields = []
+archFields = archer.Fields(archer)
+FindingsAppFields = archFields.getFieldByAppId(167)
+#print(FindingsAppFields)
+for field in FindingsAppFields:
+    #print(f"Field Id: {field['RequestedObject']['Id']}, Field Name: {field['RequestedObject']['Name']}")
+    FindingFields.append({"FieldId":field['RequestedObject']['Id'],"FieldName":field['RequestedObject']['Name']})
+
+#print(FindingFields)
+#FindingFields = FindingFields[1:]
+
+recordObject = []
+
+for recordId in recordsArray:
+    #print(f"Getting record for Id - {recordId}")
+    archRec = archer.Record(archer)
+    rec = archRec.getRecordById(recordId)
+    #print(rec['FieldContents'])
+    #fi = '2265'
+    #if fi in rec['FieldContents']:
+            #print(rec['FieldContents'][fi]['Value'])
+    for field in FindingFields:
+        id = str(field['FieldId'])
+        #print(id)
+        if id in rec['FieldContents']:
+            #print(rec['FieldContents'][id]['Value'])
+            recordObject.append({"FieldId":field['FieldId'],"FieldName":field['FieldName'],"Value":rec['FieldContents'][id]['Value']})
+        #else:
+            #print(f"Field Id {id} not found in record {recordId}")
+        
+print(recordObject)
+
+
+
+
+archContent = archer.Content(archer)
+#archContent.getContentById('Findings',348575)
